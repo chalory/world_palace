90d0755516e93f5eeff7f47ae1bce53e2926cc03
ShawnBacatan <shawnbacatan123@gmail.com>
2023-12-23 00:27:15 +0800

    style: change css layout ktv booking

--- src/components/Booking/index.tsx
+++ src/components/Booking/index.tsx
@@ -19,27 +19,25 @@ const Booking: React.FC = () => {
         <button className="room6" onClick={() => handleButtonClick('room6')}><img width="100%" src="https://via.placeholder.com/150" alt="6room" /></button>
         <button className="room7" onClick={() => handleButtonClick('room7')}><img width="100%" src="https://via.placeholder.com/150" alt="7room" /></button>
         <button className="room8" onClick={() => handleButtonClick('room8')}><img width="100%" src="https://via.placeholder.com/150" alt="8room" /></button>
-        <button className="room9" onClick={() => handleButtonClick('room9')}></button>
-        <button className="room10" onClick={() => handleButtonClick('room10')}></button>
-        <button className="room11" onClick={() => handleButtonClick('room11')}></button>
-        <button className="room12" onClick={() => handleButtonClick('room12')}></button>
-        <button className="room13" onClick={() => handleButtonClick('room13')}></button>
-        <button className="room14" onClick={() => handleButtonClick('room14')}></button>
-        <button className="room15" onClick={() => handleButtonClick('room15')}></button>
-        <button className="room16" onClick={() => handleButtonClick('room16')}></button>
+        
       </div>
       {selectedRoom && (
-        <div className="floating-form" style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>          <div style={{backgroundColor: 'beige', padding: '10px'}}>
+        <div className="floating-form" style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1, display: 'flex',}}>        
+        <div style={{backgroundColor: 'beige', padding: '10px'}}>
             <form>
               <label>
                 Name:
                 <input type="text" name="name" />
               </label>
               <label>
+                Contact number:
+                <input type="text" name='phone' />
+              </label>
+              <label>
                 Email:
                 <input type="email" name="email" />
               </label>
-              <input type="submit" value="Submit" />
+              <input className='submeet' type="submit" value="Submit"/>
             </form>
           </div>
         </div>
@@ -47,5 +45,6 @@ const Booking: React.FC = () => {
     </div>
   );
 };
+       
 
 export default Booking;
